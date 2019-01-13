a09b96e741565dd729c1d40c7e9e68c4a6e200a7
galwiner <galwiner@gmail.com>
2019-01-13 23:17:38 +0200

    cleaned up and added comments

--- main.py
+++ main.py
@@ -8,38 +8,35 @@ from matplotlib import animation, rc
 from matplotlib.gridspec import GridSpec
 from mpl_toolkits.mplot3d import Axes3D  # registers 3d perspective for scatter plot. nothing else
 
-N = 100
-T = 300
+N = 10  # number of particles in the simulation
+
+# generate a simulator object. the simulation has N particles with random position r and momentum p. both r and p are
+# drawn from a normal distribution. TODO: draw momentum from a maxwell boltzmann distribution
+
 sim = simulator(particles=[particle(np.random.normal(-0.1, 0.1, 3), np.random.normal(-1, 1, 3)) for p in range(N)],
-                forceFunc=lambda r, p: -5 * r - 0.5 * p, Tend=0.01, ownField=lambda r: 4.9 * r)
+                forceFunc=lambda r, p: -2 * r - 0.5 * p, Tend=0.01)
 (x, y, z) = sim.positions()
 
+# configure the plot
 fig = plt.figure()
-# fig2 = plt.figure()
-# ax3d = fig2.add_subplot(111, projection='3d')
 gs = GridSpec(4, 3, figure=fig)
-ax1 = fig.add_subplot(gs[0, 0])
-ax2 = fig.add_subplot(gs[0, 1])
-ax3 = fig.add_subplot(gs[0, 2])
-ax4 = fig.add_subplot(gs[1, :])
-ax5 = fig.add_subplot(gs[2, :])
-ax6=fig.add_subplot(gs[3,1],projection='3d')
+ax1 = fig.add_subplot(gs[0, 0])  # XY projection
+ax2 = fig.add_subplot(gs[0, 1])  # YZ projection
+ax3 = fig.add_subplot(gs[0, 2])  # ZX projection
+ax4 = fig.add_subplot(gs[1, :])  # line plot for the evolution of the average momentum
+ax5 = fig.add_subplot(gs[2, :])  # line plot for the evolution of the average distance from the origin
+ax6 = fig.add_subplot(gs[3, 1], projection='3d')  # 3d scatter plot of the particle positions
 fig.suptitle('Particle simulation')
 
-# ax.set_xlim3d([-1, 1])
-# ax.set_ylim3d([-1, 1])
-# ax.set_zlim3d([-1, 1])
 scat = ax1.scatter(x, y, animated=True)
-# scat3d = ax3d.scatter(x, y, z)
+scat2 = ax2.scatter(y, z, animated=True)
+scat3 = ax3.scatter(z, x, animated=True)
 
-com = sim.calc_com()
-
+com = sim.calc_com()  # calculate center of mass
 com1 = ax1.plot(com[0], com[1], 'or-', animated=True)[0]
 com2 = ax2.plot(com[1], com[2], 'or-', animated=True)[0]
 com3 = ax3.plot(com[2], com[0], 'or-', animated=True)[0]
-scat2 = ax2.scatter(y, z, animated=True)
-scat3 = ax3.scatter(z, x, animated=True)
-scat4,=ax6.plot(x,y,z,linestyle="",marker='o')
+scat4, = ax6.plot(x, y, z, linestyle="", marker='o')
 ax4.set_ylim(auto=True)
 ax5.set_ylim(auto=True)
 ax4.set_xlim(auto=True)
@@ -58,6 +55,8 @@ pplot = ax4.plot(t, avgp, avgp, animated=True)[0]
 ax4.set_ylabel('|p|')
 rplot = ax5.plot(t, avgr, animated=True)[0]
 ax5.set_ylabel('|r|')
+
+# setting up the quiver plots on ax1,2,3
 X = np.arange(-1, 1, 0.1)
 Y = np.arange(-1, 1, 0.1)
 U, V = np.meshgrid(X, Y)
@@ -66,19 +65,16 @@ meanfield2 = ax2.quiver(X, Y, U - com[1], V - com[2], scale=0.01)
 meanfield3 = ax3.quiver(X, Y, U - com[2], V - com[0], scale=0.01)
 
 
-def animate(i):
-    # ax.clear()
-    # ax4.clear()
-    # ax5.clear()
+# plt.tight_layout()
 
-    sim.run()
+
+def animate(i):
+    '''function generating the animation. it is automatically called repeatedly by FuncAnimation'''
+    sim.run()  # run one simulation step, evolving particles positions
     t.append(t[-1] + 1)
     avgp.append(sim.calc_avg_p())
     avgr.append(sim.calc_avg_r())
-    (x, y, z) = sim.positions()
-    # ax3d.clear()
-    # scat3d._offsets3d = (x,y, z)
-    # ax3d.scatter(x,y,z)
+    (x, y, z) = sim.positions()  # extract new particle positions
     ax1.set_xlim([-1, 1])
     ax1.set_ylim([-1, 1])
     ax2.set_xlim([-1, 1])
@@ -86,22 +82,15 @@ def animate(i):
     ax3.set_xlim([-1, 1])
     ax3.set_ylim([-1, 1])
 
-    # ax3d.set_xlim3d([-1, 1])
-    # ax3d.set_ylim3d([-1, 1])
-    # ax3d.set_zlim3d([-1, 1])
-    ax6.set_xlim3d([-3,3])
+    ax6.set_xlim3d([-3, 3])
     ax6.set_ylim3d([-3, 3])
     ax6.set_zlim3d([-3, 3])
-    # ax.scatter(x, y)
 
     scat.set_offsets(np.transpose(np.vstack((x, y))))
     scat2.set_offsets(np.transpose(np.vstack((y, z))))
     scat3.set_offsets(np.transpose(np.vstack((z, x))))
-    scat4.set_data(x,y)
+    scat4.set_data(x, y)
     scat4.set_3d_properties(z)
-    # ax6.scatter(x, y, z, animated=True)
-    # scat.set_array(z)
-    #  ax2.plot(t, avgp)
     pplot.set_data(t, avgp)
     rplot.set_data(t, avgr)
     ax4.set_xlim([0, max(t)])
@@ -118,9 +107,9 @@ def animate(i):
     meanfield1.set_UVC(0.00001 / (U - com[0]), 0.000001 / (V - com[1]))
     meanfield2.set_UVC(0.00001 / (U - com[1]), 0.000001 / (V - com[2]))
     meanfield3.set_UVC(0.00001 / (U - com[2]), 0.000001 / (V - com[0]))
-    return [scat, scat2, scat3, pplot, rplot, com1, com2, com3, meanfield1, meanfield2, meanfield3,scat4]
+    return [scat, scat2, scat3, pplot, rplot, com1, com2, com3, meanfield1, meanfield2, meanfield3, scat4]
 
 
-anim = animation.FuncAnimation(fig, animate, interval=1, frames=100, blit=True)
+anim = animation.FuncAnimation(fig, animate, interval=0.1, frames=100, blit=True)
 
 plt.show()
--- particlesModule.py
+++ particlesModule.py
@@ -2,40 +2,41 @@ import numpy as np
 
 
 class particle:
+    """class representing a single particle in the simulation"""
+
     def __init__(self, r=np.zeros(3), p=np.zeros(3), ownField=lambda x: np.zeros(3), m=1):
         self.r = r
         self.p = p
         self.m = 1
 
-    #         self.ownField=lambda r: np.array([0,0,0])-self.r
-    #         self.m=arc.Rubidium87.mass
-    def ownField(self,r):
+    def ownField(self, r):
+        """class defining the field emanating from a single particle"""
+
         try:
-            return 0*1 / ((r - self.r))
+            return 0 * 1 / ((r - self.r))
         finally:
-            return 0*np.random.uniform(-5,5,(1,3))
+            return 0 * np.random.uniform(-5, 5, (1, 3))
 
     def __repr__(self):
         return ("particle in r=[{0},{1},{2}],p=[{3},{4},{5}]".format(*self.r, *self.p))
 
-def gaussianBeam(k,wavelength=1):
-    '''returns the force field due to a gaussian beam with k wavevector and lambda '''
 
+def gaussianBeam(k, wavelength=1):
+    """returns the force field due to a gaussian beam with k wavevector and lambda """
     pass
 
+
 class simulator:
-    def __init__(self, particles=[], ownField=np.zeros((1, 3)), forceFunc=lambda r, p: np.array([0, 0, 0]), dt=0.01,
+    """class representing the simulation, holding many particles and evolving them in time"""
+    def __init__(self, particles=[], forceFunc=lambda r, p: np.array([0, 0, 0]), dt=0.01,
                  Tend=0.01):
         self.particles = particles
         self.Tend = Tend
         self.dt = dt
         self.forceFunc = forceFunc
-        # self.ownField = ownField
-
-    #         self.meanField=self.calcMeanField()
 
     def positions(self):
-        '''return positions of all particles'''
+        """return positions of all particles"""
         N = np.size(self.particles)
         space = np.concatenate([particle.r for particle in self.particles]).reshape(N, 3)
         return ((space[:, 0], space[:, 1], space[:, 2]))
@@ -46,17 +47,22 @@ class simulator:
     #         def meanField(r):
     #             return sigma([p.ownField for p in self.particles],r)
     #         return meanField
+
     def calc_avg_p(self):
+        """calculate the mean momentum in the simulation space"""
         return np.mean([np.linalg.norm(particle.p) for particle in self.particles])
 
     def calc_avg_r(self):
+        """calculate the mean distance from the origin"""
         return np.mean([np.linalg.norm(particle.r) for particle in self.particles])
 
     def calc_com(self):
+        """calculate the center of mass position"""
         return np.mean(self.positions(), 1)
 
     def mean_field(self, r):
-        return np.sum([particle.ownField(r) for particle in self.particles])*(np.linalg.norm(r)<0.5)
+        """calculate the mean field for particle interactions"""
+        return np.sum([particle.ownField(r) for particle in self.particles]) * (np.linalg.norm(r) < 0.5)
 
     def project(self):
         '''project the particle space onto the XZ plane'''
@@ -65,8 +71,9 @@ class simulator:
         return (x, z)
 
     def step(self):
+        """evolve the simulation by one time step"""
         for particle in self.particles:
-            particle.p = particle.p + self.dt * (self.forceFunc(particle.r, particle.p)+self.mean_field(particle.r))
+            particle.p = particle.p + self.dt * (self.forceFunc(particle.r, particle.p) + self.mean_field(particle.r))
             particle.r = particle.r + self.dt * particle.p / particle.m
 
     def run(self):
